ec4a9973ac5b53f470e77b21e13a7d070ff639f8
alfaruqii <bumbblechover@gmail.com>
2023-01-23 20:58:59 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,33 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom"
+import { Card, CardBody, CardHeader, Heading, Text } from "@chakra-ui/react"
 
 function App() {
+  const [surats, setSurats] = useState([])
+  useEffect(() => {
+    fetch("https://equran.id/api/surat/").then((res) => {
+      return res.json()
+    }).then((surat) => setSurats(surat)).catch((err) => err.message)
+  }, [])
   return (
-    <div className="flex flex-col h-full items-center justify-center bg-gray-200 text-gray-700">
-      <div className="flex items-center">
-        <h1 className="text-6xl font-thin tracking-wider">Create React App + Tailwind CSS</h1>
-      </div>
-      <p className="my-6 tracking-wide">
-        Edit <code>src/App.js</code> and save to reload.
-      </p>
-      <div className="mt-6 flex justify-center">
-        <a
-          className="uppercase hover:underline"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <a
-          className="ml-10 uppercase hover:underline"
-          href="https://tailwindcss.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Tailwind
-        </a>
-      </div>
-    </div>
+    <Router>
+      <Card className='p-10 '>
+        {surats.map((surat) => {
+          return <div className="p-5 mb-5 shadow-sm border " key={surat.nomor}>
+            <CardHeader >
+              <Heading size="md">
+                {surat.nama} ({surat.nama_latin})
+              </Heading>
+            </CardHeader>
+            <CardBody>
+              <Text>
+                <p >Artinya : {surat.arti}</p>
+                <button className='p-2 mt-1 shadow-sm rounded-sm border'>
+                  <Link to={`/surat/${surat.nomor}`}>Mulai Membaca :]</Link>
+                </button>
+              </Text>
+            </CardBody>
+          </div>
+        })}
+      </Card>
+    </Router>
   );
 }
 
